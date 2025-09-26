a31684c7905fa2302cdf75ae71bd786a1da5473e
Adam <51168566+CodeCameleon@users.noreply.github.com>
2025-09-26 23:30:32 +0200

    Added the web app assembly.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using MudBlazor.Services;
+using CodeForge3.PokerFace.WebApp.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices();
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days.
+    // You may want to change this for production scenarios,
+    // see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
