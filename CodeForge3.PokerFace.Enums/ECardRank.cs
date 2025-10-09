e22462b67afba0dc2f05204a57b51e7eca843c26
Adam <51168566+CodeCameleon@users.noreply.github.com>
2025-10-09 09:43:24 +0200

    Defined the card ranks enum.

---
+++ CodeForge3.PokerFace.Enums/ECardRank.cs
@@ -0,0 +1,72 @@
+﻿namespace CodeForge3.PokerFace.Enums;
+
+/// <summary>
+/// The enumeration of the card ranks.
+/// </summary>
+public enum ECardRank
+{
+    /// <summary>
+    /// The card rank of Two (2).
+    /// </summary>
+    Two = 2,
+    
+    /// <summary>
+    /// The card rank of Three (3).
+    /// </summary>
+    Three = 3,
+    
+    /// <summary>
+    /// The card rank of Four (4).
+    /// </summary>
+    Four = 4,
+    
+    /// <summary>
+    /// The card rank of Five (5).
+    /// </summary>
+    Five = 5,
+    
+    /// <summary>
+    /// The card rank of Six (6).
+    /// </summary>
+    Six = 6,
+    
+    /// <summary>
+    /// The card rank of Seven (7).
+    /// </summary>
+    Seven = 7,
+    
+    /// <summary>
+    /// The card rank of Eight (8).
+    /// </summary>
+    Eight = 8,
+    
+    /// <summary>
+    /// The card rank of Nine (9).
+    /// </summary>
+    Nine = 9,
+    
+    /// <summary>
+    /// The card rank of Ten (10).
+    /// </summary>
+    Ten = 10,
+    
+    /// <summary>
+    /// The card rank of Jack (J).
+    /// </summary>
+    Jack = 11,
+    
+    /// <summary>
+    /// The card rank of Queen (Q).
+    /// </summary>
+    Queen = 12,
+    
+    /// <summary>
+    /// The card rank of King (K).
+    /// </summary>
+    King = 13,
+    
+    /// <summary>
+    /// The card rank of Ace (A).
+    /// </summary>
+    Ace = 14
+}
