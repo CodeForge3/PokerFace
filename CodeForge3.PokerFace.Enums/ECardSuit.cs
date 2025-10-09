aa1697715ed67810b90e38740759a2dd165df26e
Adam <51168566+CodeCameleon@users.noreply.github.com>
2025-10-09 09:30:07 +0200

    Defined the card suits enum.

---
+++ CodeForge3.PokerFace.Enums/ECardSuit.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeForge3.PokerFace.Enums;
+
+/// <summary>
+/// The enumeration of the card suits.
+/// </summary>
+public enum ECardSuit
+{
+    /// <summary>
+    /// The card suit of Clubs (♣).
+    /// </summary>
+    Clubs = 1,
+    
+    /// <summary>
+    /// The card suit of Diamonds (♦).
+    /// </summary>
+    Diamonds = 2,
+    
+    /// <summary>
+    /// The card suit of Hearts (♥).
+    /// </summary>
+    Hearts = 3,
+    
+    /// <summary>
+    /// The card suit of Spades (♠).
+    /// </summary>
+    Spades = 4
+}
