955b5aaed46ccb35b4256d49fb346755c16b8498
OcztosKaroly <126339777+OcztosKaroly@users.noreply.github.com>
2025-10-24 14:07:11 +0200

    Create ECardCombination.cs
    Create enum, which contains every possible card combinations.

---
+++ CodeForge3.PokerFace.Enums/ECardCombination.cs
@@ -0,0 +1,57 @@
+﻿namespace CodeForge3.PokerFace.Enums;
+
+/// <summary>
+/// The enumeration of the possible card combinations.
+/// </summary>
+public enum ECardCombination
+{
+    /// <summary>
+    /// Basic card combination.
+    /// </summary>
+    HighCard,
+
+    /// <summary>
+    /// Card combination of two matching ranks.
+    /// </summary>
+    Pair,
+
+    /// <summary>
+    /// Card combination of double matching ranks. (Two pairs.)
+    /// </summary>
+    TwoPair,
+
+    /// <summary>
+    /// Card combination of three matching ranks.
+    /// </summary>
+    ThreeOfAKind,
+
+    /// <summary>
+    /// Card combination of five consecutive ranks.
+    /// </summary>
+    Straight,
+
+    /// <summary>
+    /// Card combination of five identical suits.
+    /// </summary>
+    Flush,
+
+    /// <summary>
+    /// Card combination of two and three matching ranks. (A pair and a three of a kind.)
+    /// </summary>
+    FullHouse,
+
+    /// <summary>
+    /// Card combination of four matching ranks. 
+    /// </summary>
+    FourOfAKind,
+
+    /// <summary>
+    /// Card combination of five consecutive ranks with five identical suits. (Straight and flush.)
+    /// </summary>
+    StraightFlush,
+
+    /// <summary>
+    /// Card combination of 10, Jack, Queen, King and Ace with identical suits.
+    /// </summary>
+    RoyalFlush
+}
